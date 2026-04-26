bda715aa58ec1e5d48175945e208b8d1ee512598
mdghouse <ghouse.ashrafi47@gmail.com>
2026-04-26 18:54:12 +0530

    april26

---
+++ LEETCODE/22.generate-parentheses.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=22 lang=cpp
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void solve(int open,int close,string op,vector<string> &v){
+        if(open==0 && close==0){
+            v.push_back(op);
+            return;
+        }
+        if(open!=0){
+            string op1 = op;
+            op1.push_back('(');
+            solve(open-1,close,op1,v);
+        }
+        if(open<close){
+            string op2 = op;
+            op2.push_back(')');
+            solve(open,close-1,op2,v);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> v;
+        int open = n;
+        int close = n;
+        string op = "";
+
+        solve(open,close,op,v);
+        return v;
+    }
+};
+// @lc code=end
+
